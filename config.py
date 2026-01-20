060c96fee64c02d139983fc1ef869f0837366ce3
ARJUN <pachangedinanath@gmail.com>
2026-01-20 18:51:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     API_ID="23450648"
     API_HASH="177973c53d36a5db484cabfce1b9bf2f"
     TOKEN=""
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "7651303468").split("7720230239,7487670897 "))
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "7651303468, 7720230239, 7487670897").split(""))
     START_IMG="https://files.catbox.moe/96taq6.png"
     BOT_ID=8285523518
     BOT_USERNAME="gxmassbot"
